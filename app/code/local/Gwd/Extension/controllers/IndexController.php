6de710c6a1bd9af69200ee87e107716fd1d5b93f
moonwheel <dimapetrenko.in@gmail.com>
2018-12-19 18:24:12 +0200

    end env

--- app/code/local/Gwd/Extension/controllers/IndexController.php
+++ app/code/local/Gwd/Extension/controllers/IndexController.php
@@ -5,24 +5,8 @@ class Gwd_Extension_IndexController extends Mage_Core_Controller_Front_Action
 
     public function indexAction()
     {
-
-     //   $this->loadLayout();
-     //   $this->renderLayout();
-
-        $collection = Mage::getModel('gwdextension/extension')->getCollection();
-
-        echo '<pre>';
-        var_dump($collection);
-        echo '</pre>';
-        die;
-
-
-        foreach ($collection as $value){
-
-
-
-            echo '<h1>'.$value->getTitle().'</h1>>';
-        }
+        $this->loadLayout();
+        $this->renderLayout();
     }
 
 }
